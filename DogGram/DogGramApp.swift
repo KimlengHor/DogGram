5faa773b99fce43ef9c020bc4b99a708213f1e3a
Kimleng <kimlenghor2006@gmail.com>
2023-07-04 10:50:29 -0500

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Kimleng Hor on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
